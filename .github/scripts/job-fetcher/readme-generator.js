55d948646189c3d9570311dac765073785a8abc6
z-apply <admin@zapply.jobs>
2026-05-05 01:52:15 -0500

    chore: bump submodule to 19274f9

--- .github/scripts/job-fetcher/readme-generator.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const config = require(path.join(process.cwd(), '.github/scripts/job-fetcher/config.js'));
-const jobCategories = require(path.join(process.cwd(), '.github/scripts/job-fetcher/job_categories.json'));
-const { createReadmeGenerator } = require(path.join(__dirname, '../shared/lib/readme-generator.js'));
-module.exports = createReadmeGenerator(config, jobCategories, process.cwd());
